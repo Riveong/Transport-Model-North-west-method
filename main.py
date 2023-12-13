64f670eab1f99b2ad78edb6a06deb7c3096dbc55
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-12-13 18:23:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def GUI():
     <!-- Meta tags for character set and viewport -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FastAPI Calculator</title>
+    <title>North West Calculator</title>
     <!-- Include Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
